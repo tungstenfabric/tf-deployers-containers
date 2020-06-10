4ceceac236a5ff4a0ec3a1cd533bd156f862eae0
Andrey Pavlov <andrey-mp@yandex.ru>
2020-06-10 11:41:43 +0300

    [Migration/Juniper/contrail-kolla-ansible] Change links from to tungstenfabric/tf-kolla-ansible
    Automated change
    Depends-On: I2b45a805c8b7d21b6979fc8e8412b8dda8ce244a
    
    Change-Id: I01060666b7029bbfe03aefd9881a7ac5c4b7bba6

--- containers/kolla-ansible-deployer/src/prepare.sh
+++ containers/kolla-ansible-deployer/src/prepare.sh
@@ -3,4 +3,4 @@
 cd /root
 
 [ -d contrail-ansible-deployer ] || git clone https://github.com/Juniper/contrail-ansible-deployer -b master
-[ -d contrail-kolla-ansible ] || git clone https://github.com/Juniper/contrail-kolla-ansible -b master
+[ -d contrail-kolla-ansible ] || git clone https://github.com/tungstenfabric/tf-kolla-ansible -b master
