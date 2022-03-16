858121a170f14e007ddf0f855b3afee7cb5e2225
tikitavi <akravets@juniper.net>
2022-03-16 13:54:14 +0300

    install packages with python2/3 depending on os-release
    delete python2 for ubi8 to use correct ansible python interpreter
    in case of ubi8
    
    Signed-off-by: tikitavi <akravets@juniper.net>
    Change-Id: Ia847069490406c03bc18bd7cedcb4a06b8d350e0
    (cherry picked from commit 96db2afbcd6f10afab6c0811119ce4e2c63750e2)

--- containers/kolla-ansible-deployer/src/prepare.sh
+++ containers/kolla-ansible-deployer/src/prepare.sh
@@ -4,3 +4,8 @@ cd /root
 
 [ -d contrail-ansible-deployer ] || git clone https://github.com/tungstenfabric/tf-ansible-deployer -b master contrail-ansible-deployer
 [ -d contrail-kolla-ansible ] || git clone https://github.com/tungstenfabric/tf-kolla-ansible -b master contrail-kolla-ansible
+
+source /etc/os-release
+if [[ "$ID" == 'rhel' && "${PLATFORM_ID//*:/}" == 'el8' ]] ; then
+    sed -i  '/ansible_connection: local/a \      ansible_python_interpreter: /usr/bin/python3' contrail-ansible-deployer/inventory/hosts
+fi
